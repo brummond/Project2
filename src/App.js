43e3c55d807bb4bae55998b9c80043a0fecc6b62
John Griffin <John@Johns-MacBook-Air-2.local>
2020-12-01 16:21:42 -0700

    fixed app.js error

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import firebase from '../firebase.js'
+// import firebase from 'firebase.js'
 import DisplayPost from './components/displayPost.js'
 import CreatePost from './components/createPost.js'
 import LoginPage from './components/LoginPage.js'
@@ -11,17 +11,7 @@ import {
   Route
 } from "react-router-dom";
 
-db.collection("Posts").doc("wblLiVmuuAKcLTSXSj5G").set({
-  contents: "this is a test post",
-  dates: "12-1-20",
-  names: "John"
-})
-.then(function() {
-  console.log("Document successfully written!");
-})
-.catch(function(error) {
-  console.error("Error writing document: ", error);
-});
+
 function App() {
   return (
     <Router>
