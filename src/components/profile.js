bf24834c750be9e09efdc6d1a865b629122e8f3b
John Griffin <John@Johns-MacBook-Air-2.local>
2020-12-09 11:58:27 -0700

    adding working profile page

--- src/components/profile.js
+++ src/components/profile.js
@@ -7,9 +7,15 @@ class Profile extends React.Component{
         this.state = {
             name: "",
             about: "",
-            song: "",
+            major: "",
+            minor: ""
 
         };
+    }
+    updateInput = e => {
+      this.setState({
+        [e.target.name]:e.target.value
+      });
     }
     componentDidMount()
     {
@@ -29,15 +35,28 @@ class Profile extends React.Component{
         var profileStyles = {
           textAlign: "center"
         };
+        var input = {
+          height: "40px",
+          width:"60%",
+          textAlign: "center",
+          backgroundColor: "white"
+        }
   
     return(
         <div style={profileStyles}>
         <h3>hello {this.state.name}</h3>
-        <h4>About me: </h4>
-        <h5>Major:</h5>
-        <h5>Minor:</h5>
+        <h4>About me: {this.state.about} </h4>
+        <h5>Major: {this.state.major} </h5>
+        <h5>Minor: {this.state.minor} </h5>
         <h5>Profile Picture:</h5>
         <img src = "https://via.placeholder.com/150"></img>
+        <h5>Edit Profile</h5>
+        <form>
+        {/* <input style={input} name="title" id="title" type="text"  onChange={this.updateInput} placeholder="Enter Post Title" value={this.state.title} /> */}
+          <input style = {input} name = "about" type = "text" onChange={this.updateInput} value = {this.state.about} placeholder = "About Me"/>
+          <input style = {input} name = "major" onChange={this.updateInput} value={this.state.major} placeholder = "Major"/>
+          <input style = {input} name = "minor" onChange={this.updateInput} value = {this.state.minor} placeholder = "Minor"/>
+        </form>
         </div>
         
 
