01fd06f00b25d0ae70d4f77cd8214a624567d887
lehu9098 <lehu9098@colorado.edu>
2020-12-09 08:25:43 -0700

    like is finished

--- src/components/singlePosts.js
+++ src/components/singlePosts.js
@@ -11,10 +11,13 @@ class SinglePost extends React.Component {
     this.likePost = this.likePost.bind(this);
     };
 
-  likePost = (postid) => {
+  likePost = (postid, postTitle) => {
+    document.getElementById(postTitle).style.color = "red"
     var db = firebase.firestore();
     db.collection("Posts").doc(postid).get().then((snapshot) => {
-      console.log(snapshot.data().likes)
+      var likeUp = snapshot.data().likes + 1;
+      db.collection("Posts").doc(postid).set({likes: likeUp}, { merge: true });
+      //console.log(likeUp)
     })
 
 
@@ -84,8 +87,7 @@ class SinglePost extends React.Component {
     var like = {
       backgroundColor: "transparent",
       border: "none",
-      color: "red"
-
+      color: "grey"
     }
 
     return(
@@ -93,14 +95,14 @@ class SinglePost extends React.Component {
           {this.state.posts.map((post) =>
             <div id={post.id} style={singlePost} key={post.id}>
               <div style={{display: "flex", justifyContent: "flex-end"}}>
-                <p> _ Likes</p>
+                <p> {post.likes} Likes</p>
               </div>
               <h4> {post.title}</h4>
               <h5> {post.name} </h5>
               <h6> {post.date} </h6>
               <p> {post.contents} </p>
               <div style={{display: "flex", justifyContent: "flex-end"}}>
-                <button onClick={() => this.likePost(post.id)} style={like}><i className="material-icons">favorite</i></button>
+                <button id={post.title} onClick={() => this.likePost(post.id, post.title)} style={like}><i className="material-icons">favorite</i></button>
               </div>
             </div>
           )}
